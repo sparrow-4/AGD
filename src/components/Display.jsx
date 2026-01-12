c0c9cf24198d86c67f04ea90e17cdb91ba5d1adc
sparrow-4 <thoyyibcherur@gmail.com>
2026-01-12 18:31:27 +0530

    problem clear vrcl

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Man from '../assets/man.png';
+import photo from '../assets/man.png';
 
 const Display = () => {
   return (
@@ -7,7 +7,7 @@ const Display = () => {
       <div className="max-w-8xl mx-auto px-6 md:px-20">
        
         <div className="text-center mb-5">
-          <img src={Man} alt="Man" />
+          <img src={photo} alt="Man" />
         </div>
 
        
